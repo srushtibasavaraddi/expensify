1c6319976034a5c3fd1ee9ce26a9e1785e678ecc
Srushti Basavaraddi <sbasavaraddi@gmail.com>
2020-05-12 15:23:22 +0530

    removed static adders

--- src/app.js
+++ src/app.js
@@ -11,19 +11,7 @@ import {Provider} from 'react-redux';
 //parent can change prop passed causing the child to get rendered  
 
 const store=configureStore();
-// console.log(store);
-store.dispatch(addExpense({description:'paid water bill',amount:14523}));
-store.dispatch(addExpense({description:'rent',amount:1553,createdAt:1999}));
-store.dispatch(addExpense({description:'gas bill',amount:14,createdAt:199}));
-// store.dispatch(editText('paid'));
-// console.log(store.getState());
-// setTimeout(()=>{
-//     store.dispatch(editText('rent'));
-// },2000);
 
-const state=store.getState();
-const visibleExpense=getVisibleExpense(state.expense,state.filter);
-// console.log(visibleExpense);
 const appRoot=document.getElementById("app");
 const jsx=(
     <Provider store={store}>
