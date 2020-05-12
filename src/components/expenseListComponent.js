9877b275c798b1449df13e559f7f2c1d0dcfed7a
Srushti Basavaraddi <sbasavaraddi@gmail.com>
2020-05-13 00:26:23 +0530

    formated list

--- src/components/expenseListComponent.js
+++ src/components/expenseListComponent.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {removeExpense} from '../actions/expenses';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExportListComponent=({description,amount,createdAt,dispatch,id})=>(
     <div>
         <Link to={`/edit/${id}`}>{description}</Link>
-        <p>{createdAt}-{amount}</p>
+        <p>{moment(createdAt).format('Do MMMM,YYYY')}-{numeral(amount/100).format('$0,0.00')}</p>
         <button onClick={()=>{dispatch(removeExpense({id}))}}>remove expense</button>
     </div>
 );
